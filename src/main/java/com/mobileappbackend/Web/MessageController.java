1aed2ba2a14398974f9a17e3cb1d52fb4723f5af
Mousrijhamza <mousrijhamza0@gmail.com>
2024-12-02 00:18:28 +0100

    !!

--- src/main/java/com/mobileappbackend/Web/MessageController.java
+++ src/main/java/com/mobileappbackend/Web/MessageController.java
@@ -1,5 +1,6 @@
 package com.mobileappbackend.Web;
 
+import com.mobileappbackend.amqp.RabbitMQReceiver;
 import com.mobileappbackend.amqp.RabbitMQSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,11 +12,16 @@ import org.springframework.web.bind.annotation.*;
 public class MessageController {
 
     private final RabbitMQSender rabbitMQSender;
+    private final RabbitMQReceiver rabbitMQReceiver;
 
-
-    @PostMapping
+    @PostMapping("/send")
     public String sendMessage(@RequestParam String message) {
         rabbitMQSender.sendMessage(message);
         return "Message sent: " + message;
+    }
+
+    @GetMapping("/consume")
+    public String getConsumedMessages() {
+        return rabbitMQReceiver.getMessages().toString();
     }
 }
